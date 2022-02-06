227e57966ec23202238e9e512ea8a11a202929bb
Hiroki.Igeta <h.igetta07@gmail.com>
2022-02-06 20:15:07 +0900

    Add urls

--- src/project/urls.py
+++ src/project/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^sample/', include('sample.urls', namespace='sample'))
+    re_path(r'^explore-collections/', include('sample.urls', namespace='explore-collections'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
