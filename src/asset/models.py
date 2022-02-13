b8f7902a1ea2a35c741cb3d2b07e5d3a57213110
MaybeSako <namedjobs@gmail.com>
2022-02-13 13:18:14 +0900

    add routings and pages

---
+++ src/asset/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+import uuid
+
+# Create your models here.
+class Asset(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(verbose_name='名前', max_length=40)
+    price = models.FloatField(verbose_name='価格')
+    text = models.CharField(verbose_name='説明', max_length=200, blank=True, null=False)
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='編集日時', auto_now=True)
+    artist = models.CharField(verbose_name='アーティスト', max_length=100)
+    likes = models.PositiveIntegerField(verbose_name='いいね', default=0)
